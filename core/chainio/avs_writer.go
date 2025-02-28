476b7f25f59a3ca6d496475db5bb9f9cd78c989f
oksanaphmn <oksanaphmn@gmail.com>
2025-02-28 17:10:55 +0200

    Update avs_writer.go

--- core/chainio/avs_writer.go
+++ core/chainio/avs_writer.go
@@ -111,7 +111,7 @@ func (w *AvsWriter) SendAggregatedOracleResponse(
 // 	}
 // 	receipt, err := w.TxMgr.Send(ctx, tx)
 // 	if err != nil {
-// 		w.logger.Errorf("Error submitting CreateNewTask tx")
+// 		w.logger.Errorf("Error submitting respondToTask tx")
 // 		return nil, err
 // 	}
 // 	return receipt, nil
